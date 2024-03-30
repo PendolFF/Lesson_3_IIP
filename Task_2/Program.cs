ea624cd7d58657bfa0453e6b7ed526dbc6c4fbd5
PendolFF <morev.ecrgo@gmail.com>
2024-03-30 17:26:59 +0700

    end of task2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,1 +1,17 @@
 ﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        void Square(int n)
+        {
+            int i = 0;
+            while (i <= n)
+            {
+                Console.WriteLine($"{i * i} ");
+                i++;
+            }
+        }
+        Square(Convert.ToInt32(Console.ReadLine()));
+    }
+}
