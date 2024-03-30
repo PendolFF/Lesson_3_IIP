6227bc6c2594b182cfbda9c0a4b2fe1b855e7468
PendolFF <morev.ecrgo@gmail.com>
2024-03-30 17:13:07 +0700

    end of task1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numinate = a * b;
+    int denuminate = c + d;
+    double result = numinate / denuminate;
+    Console.WriteLine(result);
+    return result;
+}
+CalculateFormula(1, 2, 3, 4);
