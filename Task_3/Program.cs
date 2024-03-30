ee3e663cdccb7c3e88a07e1aa4a30998a58c66aa
PendolFF <morev.ecrgo@gmail.com>
2024-03-30 18:16:34 +0700

    end Task3:start Task4

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void ZeroEventElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var e in arr)
+    {
+        Console.WriteLine($"{e} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+ZeroEventElements(array);
+PrintArray(array);
